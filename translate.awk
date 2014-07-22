soimort/translate-shell

commit 5f3a306111b5eafdf318d13903d4082e3c9dd2e7

new regex fixes #11

translate.awk
@@ -48,21 +48,11 @@ BEGIN {
     urlEncoding["\""] = "%22"
     urlEncoding["&"]  = "%26"
 
-    match(ARGV[1], /{.*=.*}/)
-    if (RSTART) {
-        match(ARGV[1], /{.*=/)
-        sl = substr(ARGV[1], RSTART + 1, RLENGTH - 2)
-        if (sl == "") sl = "auto"
-
-        match(ARGV[1], /=.*}/)
-        split(substr(ARGV[1], RSTART + 1, RLENGTH - 2), tls, "+")
-        if (length(tls) == 0) tls[1] = ""
-
-        text_p = 1
-    } else {
-        sl = tls[1] = "auto"
-        text_p = 0
-    }
+    match(ARGV[1], /^[{([]?([a-z][a-z](-[A-Z][A-Z])?)?=((@?[a-z][a-z](-[A-Z][A-Z])?\+)*(@?[a-z][a-z](-[A-Z][A-Z])?)?)[})\]]?$/, param)
+    text_p = RSTART
+    sl = param[1] == "" ? "auto" : param[2]
+    split(param[3], tls, "+")
+    if (length(tls) == 0) tls[1] = ""
 
     while (ARGV[++text_p]) {
         $0 = ""

commit 283ac189c666b3a6c434cda71bb625cc76c5c78c

fix a regression bug in 5f3a306

translate.awk
@@ -51,7 +51,7 @@ BEGIN {
     text_p = (ARGV[1] != "-") ? 1 : 2
     match(ARGV[text_p], /^[{([]?([a-z][a-z](-[A-Z][A-Z])?)?=((@?[a-z][a-z](-[A-Z][A-Z])?\+)*(@?[a-z][a-z](-[A-Z][A-Z])?)?)[})\]]?$/, param)
     text_p = text_p - 1 + RSTART
-    sl = param[1] == "" ? "auto" : param[2]
+    sl = param[1] == "" ? "auto" : param[1]
     split(param[3], tls, "+")
     if (length(tls) == 0) tls[1] = ""
 