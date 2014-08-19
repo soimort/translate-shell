a349b4d5febebd03b65668b2f959a31ac6b3d92d
Mort Yao <mort.yao@gmail.com>
2014-08-19 05:03:49 +0200

    add homebrew formula

---
+++ translate-shell.rb
@@ -0,0 +1,17 @@
+require "formula"
+
+class TranslateShell < Formula
+  homepage "http://www.soimort.org/translate-shell"
+  url "http://www.soimort.org/translate-shell/trans"
+  sha1 "bccc5d9dc8407aa91e40edf372005e69095cafa4"
+  version "0.8.19"
+
+  depends_on 'fribidi'
+  depends_on 'gawk'
+  depends_on 'mplayer'
+  depends_on 'rlwrap'
+
+  def install
+    bin.install "trans"
+  end
+end
