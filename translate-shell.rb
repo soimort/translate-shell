c829449f7fd20d2d0c6ea34c465d8fd6511f3b8b
Mort Yao <mort.yao@gmail.com>
2015-01-14 07:25:44 +0100

    update formula (version 0.8.22.5)

--- translate-shell.rb
+++ translate-shell.rb
@@ -2,16 +2,17 @@ require "formula"
 
 class TranslateShell < Formula
   homepage "http://www.soimort.org/translate-shell"
-  url "http://www.soimort.org/translate-shell/trans"
-  sha1 "9b1fdd87014c124b0e921d6431af63648cc3fc1c"
-  version "0.8.22.4"
+  url "http://www.soimort.org/translate-shell/translate-shell.tar.gz"
+  sha1 "9a2951ed60ae448a3ef9d9275751fa209e4db175"
+  version "0.8.22.5"
 
-  depends_on 'fribidi'
+  depends_on 'fribidi' => :optional
   depends_on 'gawk'
   depends_on 'mplayer'
   depends_on 'rlwrap'
 
   def install
     bin.install "trans"
+    man1.install "trans.1"
   end
 end
